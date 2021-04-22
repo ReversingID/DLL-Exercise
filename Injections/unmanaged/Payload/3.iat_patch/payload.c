2b2d857dbc5ac991a7e32c89120d27c0216204d0
xathrya <me@xathrya.id>
2021-04-23 01:08:19 +0700

    Fix patch_restore()

--- Injections/unmanaged/Payload/3.iat_patch/payload.c
+++ Injections/unmanaged/Payload/3.iat_patch/payload.c
@@ -1,34 +1,43 @@
 /*
-Inject DLL ini ke dalam proses.
-Mencari fungsi add, sub, dan mul di tabel IAT proses untuk kemudian
-dimodifikasi (patch)
+    IAT Patch
+    Archive of Reversing.ID
+
+    Inject DLL ini ke dalam process.
+    Mencari fungsi add, sub, dan mul di Import Address Table (IAT)
+    untuk kemudian dimodifikasi (patch).
+
+    Pemanggilan ke fungsi add, sub, dan mul akan diarahkan ke fungsi
+    di DLL.
 
 Compile:
-	[gcc]
-		(x64)
-		$ gcc -m64 -shared payload.c -o payload.dll
-		
-		(x86)
-		$ gcc -m32 -shared payload.c -o payload.dll
-
 	(msvc)
 	$ cl /nologo /LD payload.c
+
+    (clang)
+    $ clang -shared payload.c -o payload.dll
+
+    (gcc)
+    $ gcc -shared payload.c -o payload.dll
 
 Inject:
     $ injector <PID> payload.dll
 */
+
 #include <windows.h>
 #include <stdio.h>
 
 
 //========= Type Definitions ===========================================
+
 typedef struct 
 {
-    LPBYTE addr_original;
-    LPBYTE addr_hooking;
+    LPVOID addr_function;
+    LPVOID addr_original;
+    LPVOID addr_hooking;
 } IAT_ENTRY;
 
 //========= Global Variables ===========================================
+
 HMODULE  module;
 SIZE_T   imp_size;
 PIMAGE_IMPORT_DESCRIPTOR imp_desc;
@@ -39,24 +48,27 @@ IAT_ENTRY * iat_mul;
 
 //========= Helper Functions ===========================================
 
-void patch_init();
-IAT_ENTRY *  patch_it(const char* target_func, LPVOID newfunction);
-void patch_restore(IAT_ENTRY * iat_entry);
+void patch_init ();
+IAT_ENTRY *  patch_it (const char* target_func, LPVOID newfunction);
+void patch_restore (IAT_ENTRY * iat_entry);
 
 //========= Export Functions ===========================================
-int __declspec(dllexport) add(int a, int b)
+int __declspec(dllexport) 
+add (int a, int b)
 {
     printf("\nNot real ADD!\n");
 	return a + b + 2;
 }
 
-int __declspec(dllexport) sub(int a, int b)
+int __declspec(dllexport) 
+sub (int a, int b)
 {
     printf("\nNot real SUB!\n");
     return a - b + 2;
 }
 
-int __declspec(dllexport) mul(int a, int b)
+int __declspec(dllexport) 
+mul (int a, int b)
 {
     printf("\nNot real MUL!\n");
 	return a * b + 2;
@@ -88,31 +100,35 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID lpres)
 
 void patch_init()
 {
+    PIMAGE_DOS_HEADER h_dos;
+    PIMAGE_NT_HEADERS h_nt;
+
     // Mencari lokasi dari module EXE (program, bukan DLL dari program)
     module = GetModuleHandleA(NULL);
 
     // Dapatkan DOS header
-    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER) module;
+    h_dos = (PIMAGE_DOS_HEADER) module;
 
     // Dapatkan NT header dari DOS header
-    PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)((LPBYTE)dos_header + dos_header->e_lfanew);
+    h_nt  = (PIMAGE_NT_HEADERS)((LPBYTE)h_dos + h_dos->e_lfanew);
 
     // Dapatkan import descriptor dari NT header
-    imp_desc = (PIMAGE_IMPORT_DESCRIPTOR)((LPBYTE)dos_header + nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+    imp_desc = (PIMAGE_IMPORT_DESCRIPTOR)((LPBYTE)h_dos + h_nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
 
     // Informasi ukuran IAT juga diberikan oleh NT headers
-    imp_size = (SIZE_T) ((LPBYTE)dos_header + nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size);
+    imp_size = (SIZE_T) ((LPBYTE)h_dos + h_nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size);
 }
 
 IAT_ENTRY *  patch_it(const char* target_func, LPVOID newfunction)
 {
     IAT_ENTRY * retval = NULL;
+    IMAGE_IMPORT_DESCRIPTOR * iid;
 
     // Setiap file PE memiliki IMAGE_IMPORT_DESCRIPTOR masing-masing
     // Cari fungsi yang tepat dan kemudian di-patch!
     
     // Iterasi untuk setiap IMAGE_IMPORT_DESCRIPTOR (setiap DLL)
-    for (IMAGE_IMPORT_DESCRIPTOR * iid = imp_desc; iid->Name != NULL; iid++) 
+    for (iid = imp_desc; iid->Name != NULL; iid++) 
     {
         // untuk setiap DLL, iterasi fungsi
         for (int func_idx = 0; *(func_idx + (LPVOID*)(iid->FirstThunk + (SIZE_T)module)) != NULL; func_idx++) 
@@ -124,13 +140,14 @@ IAT_ENTRY *  patch_it(const char* target_func, LPVOID newfunction)
             if (!_stricmp(target_func, name))
             {
                 // Alokasi struct IAT_ENTRY baru
-                retval = (IAT_ENTRY*)malloc(sizeof(IAT_ENTRY));
+                retval = (IAT_ENTRY*) malloc(sizeof(IAT_ENTRY));
                 void** func_ptr = (func_idx + (LPVOID*)(iid->FirstThunk + (SIZE_T)module));
                 
                 DWORD oldrights, newrights = PAGE_READWRITE;
                 
                 // Dapatkan page permission dan set dengan PAGE_READWRITE agar dapat melakukan penulisan.
                 VirtualProtect(func_ptr, sizeof(LPVOID), newrights, &oldrights);
+                retval->addr_function = (LPVOID)func_ptr;
                 retval->addr_original = *func_ptr;
                 *func_ptr = newfunction;                // Tulis entry IAT dengan fungsi baru.
                 retval->addr_hooking = newfunction;
@@ -146,5 +163,13 @@ IAT_ENTRY *  patch_it(const char* target_func, LPVOID newfunction)
 
 void patch_restore(IAT_ENTRY * iat_entry)
 {
-    free(patch_it(iat_entry->addr_hooking, iat_entry->addr_original));
+    void** func_ptr = (LPVOID*)iat_entry->addr_function;
+
+    DWORD oldrights, newrights = PAGE_READWRITE;
+
+    VirtualProtect (func_ptr, sizeof(LPVOID), newrights, &oldrights);
+    *func_ptr = iat_entry->addr_original;
+    VirtualProtect (func_ptr, sizeof(LPVOID), oldrights, &newrights);
+
+    free(iat_entry);
 }
